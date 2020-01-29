b42e98c77a3258b5246a5743ffe15161252cf29a
Douglas Luman <dluman@douglasjluman.com>
2020-01-29 10:53:20 -0500

    Updating activities

---
+++ activity-03/src/main/java/activitythree/MathTrick.java
@@ -0,0 +1,16 @@
+package activitythree;
+
+/** Implements a poem in Java with one mistake.
+ *
+ * @author {Your Name Here}
+ */
+public class MathTrick {
+
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO: Program the trick!
+  }
+}
