a1205aad873d4806816ce1ac58c982fb2df5f3cc
Douglas Luman <dluman@douglasjluman.com>
2020-02-03 10:51:36 -0500

    Updating activities

---
+++ activity-04/src/main/java/activityfour/SomeCalculations.java
@@ -0,0 +1,27 @@
+package activityfour;
+
+/** Converts temperatures from Kelvin to Celsius, Celsius to Farenheit.
+ *
+ * @author {Your Name Here}
+ */
+public class SomeCalculations {
+
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    // Solve our math problem
+    // TODO: Program expression
+    
+    // Solve our temperature problem
+    int temp =  275.15;
+    // TODO: Create conversions
+    
+    /*
+     * Find out if today's temperature (in celsius) is comfortable
+     * for gators or if they're dormant!
+     */
+    double highTemp = 10.5; // High temperature in Celsius
+  }
+}
