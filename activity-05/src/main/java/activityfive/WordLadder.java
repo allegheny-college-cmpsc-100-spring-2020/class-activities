29d9c0968cf4b1c801cb6b1cece6864098567e66
Douglas Luman <dluman@douglasjluman.com>
2020-02-10 10:00:46 -0500

    Adding activities

---
+++ activity-05/src/main/java/activityfive/WordLadder.java
@@ -0,0 +1,49 @@
+package activityfive;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/** Creates and plays a word ladder game.
+ *
+ * @author {GROUP NAMES HERE}
+ */
+public class WordLadder {
+
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    // Create identifiers for input
+    File file = null;
+    Scanner input = null;
+    // Read input from file
+    try {
+      file = new File("../../../../input/words.list");
+      input = new Scanner(file);
+    } catch (FileNotFoundException noFile) {
+      System.exit(0);
+    }
+    String word = new String();
+    String startingWord = input.nextLine();
+    
+    // TODO: Create identifiers, correct casing
+    
+    // TODO: Word ladder, step 1
+    
+    // TODO: Word ladder, step 2
+    
+    // TODO: Word ladder, step 3
+    
+    // TODO: Word ladder, step 4
+    
+    // TODO: Word ladder, step 5
+    
+    // TODO: Word ladder, step 6
+    
+    // Final comparison
+    //System.out.print(word + " = " + finalWord + ": ");
+    //System.out.print(word.equals(finalWord));
+  }
+}
---
+++ sandbox/src/main/java/activities/Sandbox.java
@@ -0,0 +1,16 @@
+package activities;
+
+/** Creates a sandbox to try out code.
+ *
+ * @author Douglas Luman
+ */
+public class Sandbox {
+
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    
+  }
+}
