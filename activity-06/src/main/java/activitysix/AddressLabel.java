4fdec59795332dded52317ee6127d2ea740529e8
Douglas Luman <dluman@douglasjluman.com>
2020-02-17 09:44:23 -0500

    Updating activities

---
+++ activity-06/src/main/java/activitysix/AddressLabel.java
@@ -0,0 +1,39 @@
+package activitysix;
+
+/* 
+ * TODO: Write an import statement to import the Scanner
+ */
+ 
+/** Creates an address label.
+ *
+ * @author {Your Name Here}
+ */
+
+public class AddressLabel {
+
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    /*
+     * TODO: Set up Scanner to read inputs
+     */
+  
+    /*
+     * TODO: Take inputs
+     *       - Building number
+     *       - Street Name
+     *       - City name
+     *       - State name
+     *       - Zip code
+     */
+     
+    /*
+     * TODO: Print the above items in the format:
+     *        
+     *       {Building number} {Street name}
+     *       {City name}, {State name} {ZIP code}
+     */
+  }
+}
