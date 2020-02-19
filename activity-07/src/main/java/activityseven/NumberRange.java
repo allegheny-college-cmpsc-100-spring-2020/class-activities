9b3732cba0398b8f4940714c0bcdfbec662ca522
Douglas Luman <dluman@douglasjluman.com>
2020-02-19 10:48:18 -0500

    Updating activities

---
+++ activity-07/src/main/java/activityseven/NumberRange.java
@@ -0,0 +1,32 @@
+package activityseven;
+
+/* 
+ * TODO: Write an import statement to import:
+ *       - Scanner
+ *       - Random
+ */
+ 
+/** Generates various number ranges.
+ *
+ * @author {Your Name Here}
+ */
+
+public class NumberRange {
+
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    /*
+     * TODO: Create an instance of Random
+     */
+     
+     /*
+      * TODO: Use the following different methods of Random and print
+      *       their results:
+      *       - nextInt()
+      *       - nextDouble()
+      */
+  }
+}
